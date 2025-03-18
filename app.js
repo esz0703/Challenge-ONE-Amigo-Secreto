96569e9aed52de2ff33b60787b6dd257260aa6d9
esz0703 <duda.word@hotmail.com>
2025-03-18 03:35:04 -0300

    Alterado nome de uma variavel.

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
-let listaDeAmigos = [];
+let amigos = [];
 
 function adicionarAmigo() {
     let nomeAmigo = document.getElementById("amigo").value.trim();
     if (nomeAmigo === "") {
-        alert("Por favor, insira um nome válido.");
+        alert("Por favor, insira um nome.");
         return;
     }
-    listaDeAmigos.push(nomeAmigo);
+    amigos.push(nomeAmigo);
     document.getElementById("amigo").value = "";
     atualizarLista();
 }
@@ -14,7 +14,7 @@ function adicionarAmigo() {
 function atualizarLista() {
     let listaAmigos = document.getElementById("listaAmigos");
     listaAmigos.innerHTML = "";
-    listaDeAmigos.forEach(amigo => {
+    amigos.forEach(amigo => {
         let li = document.createElement("li");
         li.textContent = amigo;
         listaAmigos.appendChild(li);
@@ -22,12 +22,12 @@ function atualizarLista() {
 }
 
 function sortearAmigo() {
-    if(listaDeAmigos.length === 0) {
+    if(amigos.length === 0) {
         alert("Adicione pelo menos um amigo para sortear.");
         return;
     }
-    let numeroAleatorio = Math.floor(Math.random() * listaDeAmigos.length);
-    let amigoSorteado = listaDeAmigos[numeroAleatorio];
+    let numeroAleatorio = Math.floor(Math.random() * amigos.length);
+    let amigoSorteado = amigos[numeroAleatorio];
     let textoResultado = `O amigo secreto sorteado é: ${amigoSorteado}`;
     let resultado = document.getElementById("resultado");
     resultado.innerHTML = textoResultado
